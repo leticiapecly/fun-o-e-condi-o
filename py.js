5c077035c5b977096b13380695ae9f895f1bbc0b
Letícia Pecly <leticiappecly19@gmail.com>
2023-02-11 20:58:18 -0300

    função e condição

---
+++ py.js
@@ -0,0 +1,54 @@
+
+
+
+//1 - crie uma função que exiba uma mensagem no console
+    
+ function comida (pizza){
+    console.log(pizza);
+ }
+ comida ("hamburguer");
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+ function Nome(leticia){
+    console.log(leticia);
+}
+    Nome ("pecly");
+    
+    //3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+    
+    
+    function Dados (nome,idade,musica){
+
+        console.log(`Meu nome é ${nome}, minha idade é ${idade}, meu estilo musical ${musica}`);
+    }
+    Dados("leticia", 26 , "ecletico");
+
+   
+    //4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+    function Coisas (filme,musica){
+
+        console.log (`o fime que eu gosto e ${filme}, minha musica preferida e ${musica}`);
+    };
+    
+    Coisas ("Gato de botas", "ecletico"); 
+    
+    //5 - crie uma função que retorne o triplo do número recebido no parâmetro
+   
+    function Matematica(n1){
+
+    return(n1*3);
+}
+    Matematica();
+
+    console.log (Matematica(10));
+
+    
+    
+
+
+
+
+
+
